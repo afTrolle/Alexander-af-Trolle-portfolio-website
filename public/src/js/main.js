42303c77deaad0afe74b4192a191115e8d9ca8fd
afTrolle <alexander.af.trolle@gmail.com>
2019-05-13 14:51:19 +0200

    Inital commit

---
+++ public/src/js/main.js
@@ -0,0 +1,17 @@
+
+//init bootstrap code
+$(document).ready(function () { $('body').bootstrapMaterialDesign(); });
+
+//replace html to icons
+feather.replace()
+
+
+//set reveal states 
+ScrollReveal().reveal('.list-group-item', { reset: false, delay: 150, origin: 'left', interval: 100 });
+
+ScrollReveal().reveal('.sr-social', { reset: false, delay: 700, origin: 'bottom', distance: '10px', interval: 100 });
+
+ScrollReveal().reveal('.my-card-body-reveale ', { delay: 350, origin: 'left', interval: 100 });
+
+ScrollReveal().reveal('.sr-reveale', {   distance: '40px', interval: 250 });
+
